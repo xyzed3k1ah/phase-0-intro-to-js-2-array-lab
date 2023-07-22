7527066ff23be514edd8a50a89314032bc7b509c
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-21 21:54:55 -0600

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+
+function destructivelyAppendCat(name) {
+
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+
+    cats.shift(name)
+}
+
+function appendCat(name) {
+
+    const appendCats = [...cats, name];
+
+    return appendCats
+
+}
+
+function prependCat(name)  {
+
+    const prependCat = [name, ...cats];
+
+    return prependCat
+
+}
+
+function removeLastCat(name) {
+
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat() {
+
+  return cats.slice(1)
+
+
+}
